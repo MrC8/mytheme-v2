02c2623b4fd6aa12ab2716ad6e34e14da53c9c01
Boris <crik-8@hotmail.Fr>
2017-04-19 17:33:13 +0200

    Update content-single.php
    Add share elements

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -29,6 +29,10 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+        <div class="share">
+        	<a class="partage facebook" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" href="http://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>&title=<?php echo the_title(); ?>"><i class="fa fa-facebook" aria-hidden="true"></i><span>Facebook</span></a> <a class="partage google" onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" href="https://plus.google.com/share?url=<?php echo get_permalink(); ?>"><i class="fa fa-google-plus" aria-hidden="true"></i><span>Google +</span></a> <a class="partage twitter" onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" href="http://twitter.com/intent/tweet?status=<?php echo the_title(); ?>+<?php echo get_permalink(); ?>"><i class="fa fa-twitter" aria-hidden="true"></i><span>Twitter</span></a> <a class="partage linkedin" onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>&title=<?php print(urlencode(the_title())); ?>&source=<?php echo get_home_url(); ?>"><i class="fa fa-linkedin" aria-hidden="true"></i><span>Linkedin</span></a> <a href="javascript:window.print()" class="partage print"><i class="fa fa-print" aria-hidden="true"></i><span>Imprimer</span></a>
+        </div>
+		
 		<?php wiatheme_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
